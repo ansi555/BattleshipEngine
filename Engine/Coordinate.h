97461cbac33bff3a78daba9da972724e2b6dfefb
Ansgar Federbusch <a.federbusch@gmx.de>
2026-08-24 09:23:09 +0200

    Generate Coordinate

---
+++ Engine/Coordinate.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Coordinate {
+    int x;
+    int y;
+
+    Coordinate(int xVal = 0, int yVal = 0) {
+        x = xVal;
+        y = yVal;
+    }
+
+    bool equals(const Coordinate & other) {
+        if (x == other.x && y == other.y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
